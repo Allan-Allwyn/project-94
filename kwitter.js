791a01ab4c7f3782fac3ff16cfd26736ed6b9656
Allan-Allwyn <88538402+Allan-Allwyn@users.noreply.github.com>
2021-09-10 17:16:08 +0530

    Add files via upload

---
+++ kwitter.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyATREgFcjaHCi73_aEd2A6evH4f2UyD_VQ",
+    authDomain: "kwitter-project-5e67b.firebaseapp.com",
+    projectId: "kwitter-project-5e67b",
+    storageBucket: "kwitter-project-5e67b.appspot.com",
+    messagingSenderId: "612643977881",
+    appId: "1:612643977881:web:16bfb69eef09fc215e5608"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
